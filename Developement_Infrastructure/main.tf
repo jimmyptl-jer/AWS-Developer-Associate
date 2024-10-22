9eac3b70e4bf91754c2c4c6d3fbbccb558c1895a
Jimmy Patel <jimmypatel678@yahoo.com>
2024-10-22 11:19:48 -0400

    ec2 modules added

--- Developement_Infrastructure/main.tf
+++ Developement_Infrastructure/main.tf
@@ -1,9 +1,8 @@
-# main.tf
-
 provider "aws" {
   region = "us-east-1"
 }
 
+# Call VPC module to create the VPC and subnets
 module "vpc" {
   source               = "../terraform-modules/vpc"
   vpc_cidr             = "10.0.0.0/16"
@@ -13,6 +12,35 @@ module "vpc" {
   vpc_name             = "development-vpc"
 }
 
+# Call EC2 Linux module
+module "ec2_linux" {
+  source        = "../terraform-modules/ec2_linux"
+  instance_type = "t2.micro"
+  instance_name = "linux-ec2"
+
+  # Pass the first public subnet from the VPC module
+  subnet_id = element(module.vpc.public_subnet_ids, 0)
+}
+
+module "ec2_ubuntu" {
+  source        = "../terraform-modules/ec2_ubuntu"
+  instance_type = "t2.micro"
+  instance_name = "ubuntu-ec2"
+
+  # Pass the first public subnet from the VPC module
+  subnet_id = element(module.vpc.public_subnet_ids, 0)
+}
+
+
+# Output the instance ID and VPC details
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+# output "linux_ec2_instance_id" {
+#   value = module.ec2_linux.id
+# }
+
+# output "linux_ec2_instance_id" {
+#   value = module.ec2_linux.id
+# }
